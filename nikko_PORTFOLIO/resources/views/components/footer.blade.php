b7a709c3e3e342605f836ddcb830ae1ffd39445b
nikkobait <nikkodavid041p@gmail.com>
2025-11-18 17:01:59 +0800

    CC
    COPY KAY SIR

---
+++ nikko_PORTFOLIO/resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+</body>
+    <div style="width: 100%; background-color:orange" >
+        <p>Copyright 2025,</p>
+    </div>
+</html>
--- nikko_PORTFOLIO/routes/web.php
+++ nikko_PORTFOLIO/routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\AuthController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-Route::view('portfolio');
 
+Route::get('/register',[AuthController::class, 'showRegister'])->name('register.form');
+Route::post('/register',[AuthController::class, 'performRegister'])->name('register');
+
+
+Route::get('/login',[AuthController::class, 'showLogin'])->name('login.form');
+Route::post('/login',[AuthController::class, 'performLogin'])->name('login');
+
+Route::get('/logout', function () {
+});
